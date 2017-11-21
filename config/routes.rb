1046acf38e03d5a2cce06524da963eb64d04cc6f
Claire Soullie <cmp.soullie@gmail.com>
2017-11-21 14:19:37 +0100

    exo terminé (sans le front)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants do
-    resources :reviews, only: [:new, :create]
+  root to: 'restaurants#index'
+  resources :restaurants, only: [:index, :show, :new, :edit, :update, :create] do
+    resources :reviews, only: [:index, :new, :create]
   end
+  resources :reviews, only: [ :show, :edit, :create]
 end
